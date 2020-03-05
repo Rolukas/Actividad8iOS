a5b7c79a3d20f467089959e4723cb941dfba8057
Rolando <rolandosumoza@gmail.com>
2020-03-04 21:13:21 -0600

    Actividad

---
+++ Actividad8iOSRSSR/Actividad8iOSRSSR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad8iOSRSSR
+//
+//  Created by Rolando Sumoza Rivas on 3/3/20.
+//  Copyright © 2020 Rolando Sumoza Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
